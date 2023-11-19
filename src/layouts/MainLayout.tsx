95a015a1b22d3fa0e14f87071bf9441aebf2fcd5
iqbalbaharum <muhammad.iqbal.baharum@gmail.com>
2023-11-20 05:35:59 +0800

    change background, and remove build error

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
 import { Web3Wrapper } from 'App'
-import bg from '/public/img/bg.png'
+import bg from '/public/img/bg.svg'
 import AlertBox from 'components/AlertBox'
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Web3Wrapper>
-      <img src={bg} className="absolute object-cover w-screen h-screen -z-10" />
+      <img src={bg} className="absolute object-scale-down opacity-70 w-screen -z-10" />
       <Header />
       <hr className="h-px mb-8 bg-gray-700 border-0 dark:bg-gray-700" />
       <div className="container mx-auto text-white">
