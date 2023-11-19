00c1c68d9fb0d0e049ea68c1e7fb0bfcafe88520
iqbalbaharum <muhammad.iqbal.baharum@gmail.com>
2023-11-20 06:47:42 +0800

    revert to orignal slate color

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -7,7 +7,7 @@ import AlertBox from 'components/AlertBox'
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Web3Wrapper>
-      <img src={bg} className="absolute object-cover opacity-90 w-screen -z-10" />
+      <img src={bg} className="absolute object-cover w-screen -z-10" />
       <Header />
       <hr className="h-px mb-8 bg-gray-700 border-0 dark:bg-gray-700" />
       <div className="container mx-auto text-white">
